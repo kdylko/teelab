059c98771d27e266a1736cd1ac35da53fec036ec
Konstantin Dylko <kdylko@gmail.com>
2016-11-27 19:40:09 +0300

    12 commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, '/var/www/teelab'
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 set :linked_files, %w(config/database.yml config/secrets.yml)
-set :linked_dirs, %w(bin log tmp public/system)
+set :linked_dirs, %w(public/system)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
