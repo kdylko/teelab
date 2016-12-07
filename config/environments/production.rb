206ce81eaefc63f629fe79daef7f0eff0e550a0f
Konstantin Dylko <kdylko@gmail.com>
2016-12-07 15:26:28 +0300

    27 commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.yandex.com',
-    port: 465,
+    port: 587,
     domain: 'teelab.ru',
     authentication: 'plain',
     user_name: 'order@teelab.ru',
