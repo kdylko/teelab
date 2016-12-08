2f3263658e9392fc0742bf089bfa9061c8c6f673
Konstantin Dylko <kdylko@gmail.com>
2016-12-08 03:34:38 +0300

    29 commit

--- app/admin/size.rb
+++ app/admin/size.rb
@@ -1,9 +1,14 @@
 ActiveAdmin.register Size do
 
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
+
+
 permit_params :name, :gender_id, :g_name, :style_id
+
+
 #
 # or
 #
