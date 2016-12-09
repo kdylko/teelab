709e34f19e4017e36405de0dfdb6e0404ed3bb0d
Konstantin Dylko <kdylko@gmail.com>
2016-12-09 13:16:16 +0300

    37 commit

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'landing#devel' 
+  root 'landing#three' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
