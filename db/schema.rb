7cc7d0481e588675e17fe746658133d9d22dba7c
Konstantin Dylko <kdylko@gmail.com>
2016-12-09 16:49:00 +0300

    38 commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207233511) do
+ActiveRecord::Schema.define(version: 20161209133923) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -164,8 +164,9 @@ ActiveRecord::Schema.define(version: 20161207233511) do
 
   create_table "styles", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
