c2948e5df83689c07694ac776592c6ea4739e48f
Konstantin Dylko <kdylko@gmail.com>
2017-02-06 00:05:50 +0300

    Second commit

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,7 +35,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to :back }
-        format.js
+        format.js 
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'landing#three' 
+  root 'store#index' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
